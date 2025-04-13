01334a20d918d181c033e23d4a4b1950153eb2e5
raj-pwr <pawar@partikel-art.de>
2025-04-13 14:16:22 +0200

    Huge cleanup for restructuring

--- Scraper/__main__.py
+++
@@ -1,5 +0,0 @@
-from Scraper import Scraper
-
-
-def main():
-    Scraper.launch_scraper_interface()
--- Scraper/validator.py
+++
@@ -1,9 +0,0 @@
-import re
-
-def username(s):
-    
-    pattern = re.compile(r'^[a-zA-Z0-9._]{3,30}$')
-    if pattern.match(s):
-        return True, ""
-    else:
-        return False, "Username is invalid."
